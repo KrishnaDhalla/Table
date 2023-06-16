ed1e9d491b64f05963a6e759c457e050d3773ad9
Krishna <krishnadhallakd@gmail.com>
2023-06-16 17:35:47 +0530

    made some changes

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,6 @@
 import React from "react";
 import orders from "../data"
+import logo from '../Asset/hrc.jpg'
 const head = [
   "SlNo",
   "Customer Order ID",
@@ -11,12 +12,15 @@ const head = [
   "Customer Number",
   "Amount (USD)"
 ];
+
 const Table = () => {
   return (
-    <>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh",flexDirection:"column"}}>
+      <img src={logo} alt="hrc logo" style={{width:200,height:100,}} />
       <table style={{ borderCollapse: "collapse", width: "100%",backgroundColor:"	#808080" }}>
+        
         <thead>
-          <tr >
+          <tr style={{backgroundColor:"#404040"}}>
             {head.map((h) => {
               return <th style={{ border: "1px solid #dddddd", padding: "8px", color:"#ffffff" }}>{h}</th>;
             })}
@@ -28,7 +32,7 @@ const Table = () => {
                 return(
                   <tr key={index}>
                     {Object.values(order).map((val,i)=>{
-                      return <td style={{ border: "1px solid #dddddd", padding: "8px",color:"#ffffff" }}>{val}</td>
+                      return <td style={{ border: "1px solid #dddddd", padding: "8px",color:"#ffffff",textAlign:"center" }}>{val}</td>
                     })}
                   </tr>
                 )
@@ -36,7 +40,7 @@ const Table = () => {
             }
         </tbody>
       </table>
-    </>
+    </div>
   );
 };
 
